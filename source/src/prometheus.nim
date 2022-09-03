1d0932efdd850c98f037acc9b0f905c64a897f14
hugosenari <hugosenari@gmail.com>
2022-09-03 19:51:08 -0300

    feat(nimpkgs): update lock

--- source/src/prometheus.nim
+++
@@ -1,4 +0,0 @@
-import prometheus/[metric, collector, registry]
-import prometheus/collectors/[gccollector, asynccollector]
-
-export metric, collector, registry
--- source/src/prometheus/collector.nim
+++
@@ -1,7 +0,0 @@
-import metric
-
-type
-  Collector* = ref object of RootObj
-
-method collect*(self: Collector): seq[MetricFamilySamples] {.base.} =
-  doAssert false
